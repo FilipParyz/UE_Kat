02664b8e19b02c8d2e7314cf232d90a9c9ee952c
Bubbline mint <mariusz.borczuk@edu.uekat.pl>
2024-01-15 13:09:36 +0100

    Refactor SnakeGame class and add missing imports

--- Snake.py
+++ Snake.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 
-
 class Snake:
     def __init__(self, canvas, SPACE_SIZE, BODY_PARTS, SNAKE_COLOR):
         self.body_size = BODY_PARTS
@@ -82,15 +81,14 @@ class SnakeGame:
 
         # Tworzenie etykiety wyniczek
         self.label = tk.Label(
-            window, text="Wyniczek: {0}".format(self.wyniczek), font=("Comic Sans", 30)
-        ).pack()
-
+            window, text="Wyniczek: {0}".format(self.wyniczek), font=("Comic Sans", 30),fg="white", bg="black"
+        )
+        self.label.pack(fill='both', expand=True)
         self.snake = Snake(self.canvas, self.SPACE_SIZE, self.BODY_PARTS, self.SNAKE_COLOR)
         self.food = Food(self.canvas, self.SPACE_SIZE, self.FOOD_COLOR, self.GAME_WIDTH, self.GAME_HEIGHT)
 
-        self.initialize_game()
-        self.start_game()
-        #keybinds
+        
+        # keybinds
         self.window.bind("<space>", self.toggle_pause)
         self.window.bind("<BackSpace>", self.restart_game)
         self.window.bind("<Escape>", self.quit)
@@ -103,6 +101,8 @@ class SnakeGame:
         self.window.bind("s", lambda event: self.change_direction("down"))
         self.window.bind("d", lambda event: self.change_direction("right"))
 
+        self.initialize_game()
+        self.start_game()
     def initialize_game(self):
         self.window.update()
         window_width = self.window.winfo_width()
@@ -114,8 +114,6 @@ class SnakeGame:
         y = int(screen_height / 2 - window_height / 2)
 
         self.window.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
-    
 
     def start_game(self):
         self.next_turn()
@@ -131,7 +129,7 @@ class SnakeGame:
         elif new_direction == "down" and self.direction != "Up":
             self.direction = "Down"
 
-    def restart_game(self):
+    def restart_game(self, event=None):
         self.wyniczek = 0
         self.label.config(text="Wyniczek: {0}".format(self.wyniczek))
         self.canvas.delete(tk.ALL)
@@ -149,48 +147,32 @@ class SnakeGame:
             )
             self.snake.squares.append(stomach)
 
-        x = random.randint(0, (int)(GAME_WIDTH / self.SPACE_SIZE) - 1) * self.SPACE_SIZE
-        y = random.randint(0, (int)(GAME_HEIGHT / self.SPACE_SIZE) - 1) * self.SPACE_SIZE
+        x = random.randint(0, (int)(self.GAME_WIDTH / self.SPACE_SIZE) - 1) * self.SPACE_SIZE
+        y = random.randint(0, (int)(self.GAME_HEIGHT / self.SPACE_SIZE) - 1) * self.SPACE_SIZE
         self.food.coordinates = [x, y]
-        self.canvas.create_polygon(
-            x + self.SPACE_SIZE / 2,
-            y,
-            x + self.SPACE_SIZE,
-            y + self.SPACE_SIZE / 3,
-            x + 3 * self.SPACE_SIZE / 4,
-            y + self.SPACE_SIZE,
-            x + self.SPACE_SIZE / 4,
-            y + self.SPACE_SIZE,
-            x,
-            y + self.SPACE_SIZE / 3,
-            fill=self.FOOD_COLOR,
-            tag="Jedzonko",
+        self.canvas.create_rectangle(
+            x, y, x + self.SPACE_SIZE, y + self.SPACE_SIZE, fill=self.FOOD_COLOR, tag="Jedzonko"
         )
 
         self.window.bind("<Escape>", lambda event: self.toggle_pause())
 
     def quit(self, event):
-        self.root.destroy()
+        self.window.destroy()
         self.pop_up = tk.Toplevel()
         self.pop_up.title("Snakey")
         self.pop_up.resizable(False, False)
         self.pop_up.geometry("300x100")
         self.pop_up_label = tk.Label(
-            self.pop_up, text=f"Do you want to end game with score: {score}?", font=("Comic Sans", 30)
-        ).pack()
-        self.pop_up_button = tk.Button(self.pop_up, text="Yes", command=self.root.destroy).pack()
-        self.pop_up_button = tk.Button(self.pop_up, text="No", command=self.pop_up.destroy).pack()
+            self.pop_up, text=f"Do you want to end game with score: {self.wyniczek}?", font=("Comic Sans", 14)
+        )
+        self.pop_up_label.pack()
+        self.pop_up_button_yes = tk.Button(self.pop_up, text="Yes", command=self.window.destroy)
+        self.pop_up_button_yes.pack()
+        self.pop_up_button_no = tk.Button(self.pop_up, text="No", command=self.pop_up.destroy)
+        self.pop_up_button_no.pack()
 
-    def toggle_pause(self):
+    def toggle_pause(self, event=None):
         self.paused = not self.paused
-
-    def pause_game(self):
-        if not self.paused:
-            self.paused = True
-            self.window.bind("<Escape>", lambda event: self.toggle_pause())
-        else:
-            self.paused = False
-            self.window.unbind("<Escape>")
 
     def check_collisions(self):
         x, y = self.snake.coordinates[0]
@@ -207,9 +189,9 @@ class SnakeGame:
 
         if str1 == "Ściana!":
             self.canvas.create_text(
-                self.canvas.winfo_width() / 2,
-                self.canvas.winfo_height() / 2,
-                font=("Comic Sans", 80),
+                self.GAME_WIDTH / 2,
+                self.GAME_HEIGHT / 2,
+                font=("Comic Sans", 30),
                 text=f"({str1})",
                 fill="purple",
                 tag="gameover",
@@ -222,9 +204,9 @@ class SnakeGame:
                 self.window.destroy()
         elif str1 == "Zjadł siebie!":
             self.canvas.create_text(
-                self.canvas.winfo_width() / 2,
-                self.canvas.winfo_height() / 2,
-                font=("Comic Sans", 80),
+                self.GAME_WIDTH / 2,
+                self.GAME_HEIGHT / 2,
+                font=("Comic Sans", 30),
                 text=f"{str1}",
                 fill="purple",
                 tag="gameover",
@@ -237,9 +219,9 @@ class SnakeGame:
                 self.window.destroy()
         else:
             self.canvas.create_text(
-                self.canvas.winfo_width() / 2,
-                self.canvas.winfo_height() / 2,
-                font=("Comic Sans", 80),
+                self.GAME_WIDTH / 2,
+                self.GAME_HEIGHT / 2,
+                font=("Comic Sans", 30),
                 text=f"Przegrana ;(",
                 fill="purple",
                 tag="gameover",
@@ -249,7 +231,7 @@ class SnakeGame:
 
     def next_turn(self):
         if self.paused:
-            self.window.after(self.Speed, self.next_turn)
+            self.window.after(self.SPEED, self.next_turn)
             return
 
         x, y = self.snake.coordinates[0]
@@ -272,7 +254,7 @@ class SnakeGame:
             self.wyniczek += 1
             self.label.config(text="Wyniczek: {0}".format(self.wyniczek))
             self.canvas.delete("Jedzonko")
-            self.food = Food(self.canvas, self.SPACE_SIZE, self.FOOD_COLOR)
+            self.food = Food(self.canvas, self.SPACE_SIZE, self.FOOD_COLOR, self.GAME_WIDTH, self.GAME_HEIGHT)
         else:
             del self.snake.coordinates[-1]
             self.canvas.delete(self.snake.squares[-1])
